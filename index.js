32c73eb6fe1d9911299a8eafa47b2664cd334ba1
pblanco27-soin <pblanco@soin.co.cr>
2021-08-25 23:50:40 -0600

    Clustering examples

---
+++ index.js
@@ -0,0 +1,26 @@
+const cluster = require("cluster");
+
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but
+  // in child mode
+  cluster.fork();
+} else {
+  // I'm a child, i'm going to act like a server
+  // and do nothing else
+  const express = require("express");
+  const crypto = require("crypto");
+  const app = express();
+
+  app.get("/", (req, res) => {
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+      res.send("Hi there");
+    });
+  });
+
+  app.get("/fast", (req, res) => {
+    res.send("This was fast");
+  });
+
+  app.listen(3000);
+}
